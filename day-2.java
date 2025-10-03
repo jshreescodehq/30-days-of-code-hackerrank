608a7c300763d5e52b676f4d42e4fcf046f8fa68
jshreescodehq <jaya04092006shree@gmail.com>
2025-10-03 12:12:06 +0530

    Day 2: Operators

--- day-2.java
+++ day-2.java
@@ -8,7 +8,6 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-
 class Result {
      public static void solve(double meal_cost, int tip_percent, int tax_percent) {
         double tip = (tip_percent * meal_cost)/100.0;
@@ -16,9 +15,7 @@ class Result {
         double total_cost= meal_cost + tip + tax;
         System.out.println(Math.round(total_cost));
     }
-
 }
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
