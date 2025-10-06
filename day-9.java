a1cd4330de8e12ef2049c4cb358d4c30e8135b17
jshreescodehq <jaya04092006shree@gmail.com>
2025-10-06 19:50:03 +0530

    Day 9: Recursion 3

--- day-9.java
+++ day-9.java
@@ -10,7 +10,6 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
-
 class Result {
     public static int factorial(int n) {
         if(n<=1){
