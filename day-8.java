feade589bf24c2be49ac72935a1e746d4f8cfb02
jshreescodehq <jaya04092006shree@gmail.com>
2025-10-06 19:48:58 +0530

    Day 8: Dictionaries and Maps

--- day-8.java
+++ day-8.java
@@ -1,4 +1,4 @@
-/*Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers.
+/*Given n names and phone numbers, assembled a phone book that maps friends' names to their respective phone numbers.
 You will then be given an unknown number of names to query your phone book for. 
 For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; if an entry for name is not found, print Not found instead. */
 
