e58e491fa1b82ca8b3b54847f29e4cf949a61076
jshreescodehq <jaya04092006shree@gmail.com>
2025-10-07 19:50:50 +0530

    Day 10: Binary Numbers

--- day-10.java
+++ day-10.java
@@ -1,6 +1,6 @@
-//Given a base-10 integer n , convert it to binary (base-2).
-//Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation. 
-//When working with different bases, it is common to show the base as a subscript.
+// Given a base-10 integer n , convert it to binary (base-2).
+// Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation. 
+// When working with different bases, it is common to show the base as a subscript.
 
 import java.io.*;
 import java.math.*;
